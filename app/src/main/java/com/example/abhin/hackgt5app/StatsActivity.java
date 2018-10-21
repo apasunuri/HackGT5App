709f1363ea657eb855b31dd5c766c06b445703d8
leetageltser <leeta@geltser.com>
2018-10-21 07:24:06 -0400

    Preparing to merge

--- app/src/main/java/com/example/abhin/hackgt5app/StatsActivity.java
+++ app/src/main/java/com/example/abhin/hackgt5app/StatsActivity.java
@@ -1,7 +1,9 @@
 package com.example.abhin.hackgt5app;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class StatsActivity extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class StatsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stats);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.app_bar);
+        setSupportActionBar(toolbar);
+        ActionBar ab = getSupportActionBar();
+        ab.setDisplayHomeAsUpEnabled(true);
     }
 }
