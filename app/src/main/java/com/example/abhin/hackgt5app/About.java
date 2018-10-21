0c05bffe9c6f8217e140d52bada8aa67a52a2118
leetageltser <leeta@geltser.com>
2018-10-21 07:13:46 -0400

    about to merge

---
+++ app/src/main/java/com/example/abhin/hackgt5app/About.java
@@ -0,0 +1,13 @@
+package com.example.abhin.hackgt5app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class About extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+    }
+}
